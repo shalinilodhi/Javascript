42dd10e913292501f3a6e2921b07cb8d5f619477
shalinilodhi <136580060+shalinilodhi@users.noreply.github.com>
2026-08-30 15:10:29 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountID = 12345
+let accountMail = "shalini@12345"
+var accountPassword = "12233"
+//accountID = 2  not allowed
+console.log(accountID);
+console.table([accountID,accountMail,accountPassword])
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict"; // treat all js code as newer version 
+/* 
+Primitive data type 
+number 
+bigint
+string 
+boolean
+null 
+undefined
+symbol
+ */
+console.log(typeof("Shalini"))
+console.log(typeof null)
+console.log(typeof undefined)
