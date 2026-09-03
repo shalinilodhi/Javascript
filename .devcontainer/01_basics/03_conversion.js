07443f16c77082f2aebf9584f8a0c3e996f1fdfb
shalinilodhi <136580060+shalinilodhi@users.noreply.github.com>
2026-09-03 06:49:22 +0000

    opeartions in js

--- .devcontainer/01_basics/03_conversion.js
+++ .devcontainer/01_basics/03_conversion.js
@@ -1,4 +1,10 @@
-let score = "shalini";
-let s = Boolean(score)
-console.log(typeof s)
-console.log(s)
+// console.log("1" +2+2+4)
+// console.log(1+2+3+"4")
+// console.log(1+2+3+"4"+3+5+6)
+//  console.log(null==0) 
+//  console.log(null>0)
+//  console.log(null<=0) relational operation treats null as 0
+
+console.log(undefined==0)
+console.log(undefined>0)
+console.log(undefined<=0)
